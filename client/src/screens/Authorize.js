b9d7d91b73db41d18d28ac366c7a9863a4f3b231
David Alvarez <david@southfloridaathleticclub.com>
2022-05-15 01:13:38 -0400

    finished oauth loading screens but need error handling

--- client/src/screens/Authorize.js
+++ client/src/screens/Authorize.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import { Box } from '@chakra-ui/layout';
+import { Spinner } from '@chakra-ui/spinner';
+import React, { useEffect } from 'react';
+import { REQUEST_TOKEN_ROUTE } from '../constants';
+import useFetch from '../hooks/useFetch';
 
 export default function Authorize() {
+  const { makeRequest } = useFetch();
+  useEffect(() => {
+    makeRequest(
+      {
+        url: `${REQUEST_TOKEN_ROUTE}`,
+        method: 'POST',
+        body: {
+          scope: `https://www.googleapis.com/auth/webmasters`,
+        },
+      },
+      res => {
+        if (res.data.data) {
+          window.location.href = res.data.data;
+        }
+      }
+    );
+  }, []);
+
   return (
-    <div>Authorize</div>
-  )
+    <Box
+      style={{
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        fontSize: 40,
+        fontFamily: 'Georgia',
+      }}
+    >
+      Authorizing...
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Box>
+  );
 }
--- client/src/screens/OAuth.js
+++ client/src/screens/OAuth.js
@@ -1,8 +1,16 @@
+import { Box } from '@chakra-ui/layout';
+import { Spinner } from '@chakra-ui/spinner';
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { GET_ACCESS_TOKEN_ROUTE } from '../constants';
+import useFetch from '../hooks/useFetch';
+import { useNavigate } from 'react-router';
 
 export default function OAuth2() {
   const [code, setCode] = useState('');
+  const [text, setText] = useState('Loading...');
+  const { makeRequest } = useFetch();
+  const navigate = useNavigate();
   let [searchParams] = useSearchParams();
   useEffect(() => {
     searchParams.forEach((val, key) => {
@@ -11,27 +19,45 @@ export default function OAuth2() {
       }
     });
     if (code.length > 0) {
-      /* fetch(process.env.REACT_APP_AUTH_API + "token", {
-        method: "POST",
-        body: JSON.stringify({
-          code: code,
-          scope: 'https://www.googleapis.com/auth/webmasters',
-        }),
-        headers: { "Content-Type": "application/json" },
-      }); */
+      makeRequest(
+        {
+          url: GET_ACCESS_TOKEN_ROUTE,
+          method: 'POST',
+          body: {
+            code: code,
+            scope: 'https://www.googleapis.com/auth/webmasters',
+          },
+        },
+        res => {
+          if (res.data.data) {
+            setText('Redirecting...');
+            navigate('/');
+          }
+        }
+      );
     }
   }, [code]);
   return (
-    <div
+    <Box
       style={{
         height: '100vh',
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'column',
+        fontSize: 40,
+        fontFamily: 'Georgia',
       }}
     >
-      <p style={{ fontSize: 32, fontFamily: 'Georgia' }}>{'...Loading'}</p>
-    </div>
+      {text}
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Box>
   );
 }
