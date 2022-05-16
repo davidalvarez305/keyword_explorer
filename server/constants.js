4ac7a2885cea8fa12910b7930f3366a1ac6e4a5c
David Alvarez <david@southfloridaathleticclub.com>
2022-05-16 07:19:23 -0400

    had to make some adjustments for prod

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,5 @@
 import "dotenv/config"
-import * as config from "./google.json";
 export const __prod__ = process.env.NODE_ENV === "production";
 export const PORT = process.env.PORT || 4010;
-export const REDIRECT_URI = __prod__ ? config.default.web.redirect_uris[1] : config.default.web.redirect_uris[0];
+export const REDIRECT_URI = process.env.REDIRECT_URI;
 export const COOKIE_NAME = process.env.COOKIE_NAME;
