1e84d474af3c26e2798a5be84f577f6a523b48fa
David Alvarez <david@southfloridaathleticclub.com>
2022-05-17 18:36:44 -0400

    trying to get cookie to workl

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ const index = async () => {
         httpOnly: true,
         secure: __prod__,
         sameSite: "strict",
-        domain: __prod__ ? `.${domain.hostname}` : undefined,
+        domain: __prod__ ? `${domain.hostname.hostname.replace("www.", ".")}` : undefined,
       },
     })
   );
